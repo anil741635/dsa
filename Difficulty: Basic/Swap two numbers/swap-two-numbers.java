66ebc3b07747fe9ad6fce61a506bc856f65ad518
anil741635 <anil23hr1a0547@gmail.com>
2026-01-21 10:24:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap two numbers/swap-two-numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    static List<Integer> get(int a, int b) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        list.add(a);
+        list.add(b);
+        return list;
+    }
+}
