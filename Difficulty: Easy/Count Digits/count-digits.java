ddc940cce6f0f25c323f39d1a7593df5b47c1d21
anil741635 <anil23hr1a0547@gmail.com>
2026-02-14 19:45:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -1,11 +1,15 @@
+// User function Template for Java
+
 class Solution {
-    public int countDigits(int n) {
+    static int evenlyDivides(int n) {
         // code here
         int count=0;
+        int temp=n;
         while(n>0){
-            int last=n/10;
-            count++;
-            n=last;
+            int last=n%10;
+            if(last!=0)
+                if(temp%last==0) count++;
+            n/=10;
         }
         return count;
     }
