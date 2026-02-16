8861bdc6be8e1cfc601911b553176a00dd244f0f
anil741635 <anil23hr1a0547@gmail.com>
2026-02-16 20:40:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/All numbers with specific difference/all-numbers-with-specific-difference.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int getCount(int n, int d) {
+        // code here
+        int count=0;
+        int high=n,low=d;
+        int ans=-1;
+        while(low<=high){
+              int mid=low+(high-low)/2;
+              int value=sumOfDigits(mid);
+              if((mid-value)>=d) {
+                  ans=mid;
+                  high=mid-1;
+              }
+                else{
+                    low=mid+1;
+                }  
+         }
+         if(ans==-1) return 0;
+      return n-ans+1;
+    }
+    public static int sumOfDigits(int d){
+        int sum=0;
+        while(d>0){
+            sum+=d%10;
+            d/=10;
+        }
+        return sum;
+    }
+}
