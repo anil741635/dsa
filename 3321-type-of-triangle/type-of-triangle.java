ce0859a9569175902ca296117cbafb2b273bb1a1
anil741635 <anil23hr1a0547@gmail.com>
2026-03-01 19:57:50 +0530

    Time: 3 ms (9.93%) | Memory: 44 MB (18.82%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[0]+nums[1]<=nums[2]) return "none";
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums) set.add(i);
+        if(set.size()==1) return "equilateral";
+        else if(set.size()==2) return "isosceles";
+        else return "scalene";
+    }
+}
