705641a29b7271df980debb48d220e4ace530ed4
anil741635 <anil23hr1a0547@gmail.com>
2026-03-03 22:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (55.63%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int maxi=nums[0];
+       int  index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>maxi) {
+                maxi=nums[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+}
