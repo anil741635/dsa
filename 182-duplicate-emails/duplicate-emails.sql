38d2a70f1e0a44aacf49d871ddf73e2815100ff2
anil741635 <anil23hr1a0547@gmail.com>
2026-03-09 20:45:18 +0530

    Time: 396 ms (26.07%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select  email as Email from Person group by email having count(email)>1;
+select distinct e1.email as Email from Person e1,Person e2 where e1.email=e2.email and e1.id>e2.id;
