c48c4c1eadb1d8df00e4d219b3eafafe161b4f7e
anil741635 <anil23hr1a0547@gmail.com>
2026-03-15 08:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int firstRepeated(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]) return i+1;
+            }
+        }
+        return -1;
+    }
+}
