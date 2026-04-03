c13db1007bfdffcad8ccda996743415b3aa248e0
anil741635 <anil23hr1a0547@gmail.com>
2026-04-03 09:11:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue Using Array/queue-using-array.java
@@ -0,0 +1,50 @@
+class myQueue {
+    int[] arr;
+    int capacity, front, rear, size;
+
+    // Constructor
+    public myQueue(int n) {
+        capacity = n;
+        arr = new int[n];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public void enqueue(int x) {
+        if (isFull()) {
+            return; // avoid printing (important for judge)
+        }
+
+        rear = (rear + 1) % capacity;  // 🔥 circular move
+        arr[rear] = x;
+        size++;
+    }
+
+    public void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+
+        front = (front + 1) % capacity; // 🔥 circular move
+        size--;
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return arr[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return arr[rear];
+    }
+}
