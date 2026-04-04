25ec8fd5fab1c3418516b727cb80d8d29971ed17
anil741635 <anil23hr1a0547@gmail.com>
2026-04-04 22:21:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -0,0 +1,22 @@
+class Solution {
+    ArrayList<Integer> find(int arr[], int x) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+       int first=-1,second=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                first=i;
+                break;
+            }
+        }
+        list.add(first);
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]==x){
+                second=i;
+                break;
+            }
+        }
+        list.add(second);
+        return list;
+    }
+}
