fff2dc0d202809b1397da1117a3b60c2d3306419
anil741635 <anil23hr1a0547@gmail.com>
2026-04-10 11:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (34.36%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        double ans=1;
+        for(int i=1;i<m;i++){
+            ans=ans*(n-1+i)/i;
+        }
+        return (int) ans;
+    }
+}
