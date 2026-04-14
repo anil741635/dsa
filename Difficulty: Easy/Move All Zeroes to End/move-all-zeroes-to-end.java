b2df596ed0f946fb8642a7e7b0c86a12ad7f0b36
anil741635 <anil23hr1a0547@gmail.com>
2026-04-14 15:37:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -1,17 +1,17 @@
 class Solution {
     void pushZerosToEnd(int[] arr) {
         // code here
+        int arr1[]=new int[arr.length];
         int j=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]!=0){
-                swap(i,j,arr);
+                arr1[j]=arr[i];
                 j++;
-            }
+            } 
         }
-    }
-    public static void swap(int i,int j,int[] arr){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+        for(int i=0;i<arr1.length;i++){
+            arr[i]=arr1[i];
+            
+        }
     }
 }
