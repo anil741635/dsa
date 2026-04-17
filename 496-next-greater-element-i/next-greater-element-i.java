17fbb62dadfd56ed9a191ea8d29cc841aa2a6a3a
anil741635 <anil23hr1a0547@gmail.com>
2026-04-17 14:53:33 +0530

    Time: 5 ms (15.77%) | Memory: 44.7 MB (87.08%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] arr=new int[nums2.length];
-        Stack<Integer> st=new Stack<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!st.isEmpty() && nums2[i]>=st.peek()){
-                st.pop();
-            }
-            arr[i]=st.isEmpty() ? -1:st.peek();
-            st.push(nums2[i]);
+        int arr[]=new int[nums2.length];
+       Stack<Integer> st=new Stack<>();
+       for(int i=nums2.length-1;i>=0;i--){
+        while(!st.isEmpty() && nums2[i]>=st.peek()){
+            st.pop();
         }
-         int[] result=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                        result[i]=arr[j];
-                        break;
-                }
+        arr[i]=st.isEmpty()? -1:st.peek();
+        st.push(nums2[i]);
+       }
+       for(int i=0;i<nums1.length;i++){
+        for(int j=0;j<nums2.length;j++){
+            if(nums1[i]==nums2[j]) {
+                nums1[i]=arr[j];
+                break;
             }
         }
-        return result;
+       }
+       return nums1;
     }
 }
