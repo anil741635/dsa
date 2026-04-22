af05e324603fb2f8c8b41f3a7f53064b9a1d24c2
anil741635 <anil23hr1a0547@gmail.com>
2026-04-22 13:03:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Postorder Traversal/postorder-traversal.java
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.java
@@ -13,13 +13,18 @@ class Solution {
     public ArrayList<Integer> postOrder(Node root) {
         // code here
         ArrayList<Integer> list=new ArrayList<>();
-        traverse(root,list);
-        return list;
-    }
-    public static void traverse(Node root,ArrayList<Integer> list){
-        if(root==null) return;
-        traverse(root.left,list);
-        traverse(root.right,list);
-        list.add(root.data);
+        Stack<Node> st1=new Stack<>();
+       Stack<Node> st2=new Stack<>();
+       st1.push(root);
+       while(!st1.isEmpty()){
+           Node curr=st1.pop();
+           st2.push(curr);
+           if(curr.left!=null) st1.push(curr.left);
+           if(curr.right!=null) st1.push(curr.right);
+       }
+       while(!st2.isEmpty()){
+           list.add(st2.pop().data);
+       }
+       return list;
     }
 }
