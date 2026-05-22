8ce90e986f4c5c8f748dca90c5ebe65e70eb80ab
anil741635 <anil23hr1a0547@gmail.com>
2026-05-22 20:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (21.65%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,19 +3,20 @@ class Solution {
         int n=nums.length; 
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
-        int[] dp1=new int[n];
-        int[] dp2=new int[n];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        int firstone=helper(nums,0,n-2,dp1); 
-        int secondone=helper(nums,1,n-1,dp2);
-        return Math.max(firstone,secondone);
-    }
-    public static int helper(int[] nums,int start,int end,int[] dp){
-        if(start>end) return 0;
-        if(dp[start]!=-1) return dp[start];
-        int skip=helper(nums,start+1,end,dp);
-        int notskip= nums[start]+helper(nums,start+2,end,dp);
-        return dp[start]=Math.max(skip,notskip);
+        int dp1[]=new int[n-1];
+        dp1[0]=nums[0];
+        dp1[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length-1;i++){
+            dp1[i]=Math.max(dp1[i-1],nums[i]+dp1[i-2]);
+        }
+        int firstmax=dp1[n-2];
+        int dp2[]=new int[n];
+        dp2[1]=nums[1];
+        dp2[2]=Math.max(nums[1],nums[2]);
+        for(int i=3;i<nums.length;i++){
+            dp2[i]=Math.max(dp2[i-1],nums[i]+dp2[i-2]);
+        }
+        int secondmax=dp2[n-1];
+        return Math.max(firstmax,secondmax);
     }
 }
