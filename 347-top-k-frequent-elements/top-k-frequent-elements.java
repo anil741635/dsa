4fc5875cb5f467213a1b919a1c5b9801e322ed3b
anil741635 <anil23hr1a0547@gmail.com>
2026-05-23 11:26:34 +0530

    Time: 15 ms (54.78%) | Memory: 47.2 MB (94.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        ArrayList<Integer> ans=new ArrayList<>();
         Map<Integer,Integer> map=new HashMap<>();
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        List<Integer>[] bucket=new List[nums.length+1];
-        for(int num:map.keySet())
-        {
-            int freq=map.get(num);
-            if(bucket[freq] ==null)
-            {
-                bucket[freq]=new ArrayList();
-            }
-            bucket[freq].add(num);
-        }
-        int[] res=new int[k];
-        int index=0;
-        for(int i=bucket.length-1;i>=0 && index<k;i--)
-        {
-            if(bucket[i]!=null){
-                for(int j:bucket[i]){
-                    res[index++]=j;
-                    if(index==k)
-                    {
-                        break;
-                }    }
+        List<Map.Entry<Integer, Integer>> list =new ArrayList<>(map.entrySet());
+
+        Collections.sort(list,(a, b) -> b.getValue().compareTo(a.getValue()));
+        for(int i=0;i<list.size();i++){
+            if(k>0){
+                ans.add(list.get(i).getKey());
+                k--;
             }
         }
-        return res;
+        int[] arr=new int[ans.size()];
+        for(int i=0;i<ans.size();i++) arr[i]=ans.get(i);
+        return arr;
     }
 }
