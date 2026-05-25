ea38717d04d9535a659cfbfbb07ef3b5a9e35e6e
anil741635 <anil23hr1a0547@gmail.com>
2026-05-25 13:44:23 +0530

    Time: 7 ms (65.02%) | Memory: 46.7 MB (26.64%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,20 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums,target,0,0);
+        int total=0;
+        for(int i:nums) total+=i;
+        if(total<Math.abs(target)) return 0;
+        int dp[][]=new int[nums.length+1][2*total+1];
+        for(int i[]:dp) Arrays.fill(i,-1);
+        return helper(nums,target,0,0,dp,total);
     }
-    public static int helper(int[] nums,int target,int index,int sum){
+    public static int helper(int[] nums,int target,int index,int sum,int dp[][],int s1){
         if(index==nums.length) {
             if(sum==target) return 1;
             return 0;
         }
-        int pick=helper(nums,target,index+1,sum+nums[index]);
-        int notpick=helper(nums,target,index+1,sum-nums[index]);
-        return pick+notpick;
+        if(dp[index][sum+s1]!=-1) return dp[index][sum+s1];
+        int pick=helper(nums,target,index+1,sum+nums[index],dp,s1);
+        int notpick=helper(nums,target,index+1,sum-nums[index],dp,s1);
+        return dp[index][sum+s1]=pick+notpick;
     }
 }
