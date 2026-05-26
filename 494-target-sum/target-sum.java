dacef8fa942a9fcd8b06cf169bb695e818881819
anil741635 <anil23hr1a0547@gmail.com>
2026-05-26 19:41:26 +0530

    Time: 6 ms (71.40%) | Memory: 45.1 MB (36.70%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -3,18 +3,16 @@ class Solution {
         int total=0;
         for(int i:nums) total+=i;
         if(total<Math.abs(target)) return 0;
-        int dp[][]=new int[nums.length+1][2*total+1];
-        for(int i[]:dp) Arrays.fill(i,-1);
-        return helper(nums,target,0,0,dp,total);
-    }
-    public static int helper(int[] nums,int target,int index,int sum,int dp[][],int s1){
-        if(index==nums.length) {
-            if(sum==target) return 1;
-            return 0;
+        int s1=(target+total)/2;
+        if((target + total) % 2 != 0) return 0;
+        int dp[][]=new int[nums.length+1][s1+1];
+        dp[0][0]=1;
+       for(int i=1;i<=nums.length;i++){
+        for(int j=0;j<=s1;j++){
+            dp[i][j]=dp[i-1][j];
+            if(nums[i-1]<=j) dp[i][j]+=dp[i-1][j-nums[i-1]];
         }
-        if(dp[index][sum+s1]!=-1) return dp[index][sum+s1];
-        int pick=helper(nums,target,index+1,sum+nums[index],dp,s1);
-        int notpick=helper(nums,target,index+1,sum-nums[index],dp,s1);
-        return dp[index][sum+s1]=pick+notpick;
+       }
+       return dp[nums.length][s1];
     }
 }
