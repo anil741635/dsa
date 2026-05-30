cd78dc1831107db28f6a961dd789b860d524338b
anil741635 <anil23hr1a0547@gmail.com>
2026-05-30 08:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (96.02%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int lb=lowerBound(nums,target);
-        int ub=upperBound(nums,target);
-         int ans[]={lb,ub};
-         return ans;
+        int start=lowerBound(nums,-1,target);
+        int end=upperBound(nums,-1,target);
+        return new int[] {start,end};
     }
-    public int lowerBound(int nums[],int target){
-       int start=0,end=nums.length-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
+    public static int lowerBound(int[] nums,int index,int target){
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
-                ans=mid;
-                end=mid-1;
+                index=mid;
+                right=mid-1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if(nums[mid]>target){
+                right=mid-1;
             }
-            else{
-                end=mid-1;
-            }
+            else left=mid+1;
         }
-        return ans;
+        return index;
     }
-
-     public int upperBound(int nums[],int target){
-       int start=0,end=nums.length-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
+    public static int upperBound(int[] nums,int index,int target){
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
-                ans=mid;
-                start=mid+1;
+                index=mid;
+                left=mid+1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if(nums[mid]>target){
+                right=mid-1;
             }
-            else{
-                end=mid-1;
-            }
+            else left=mid+1;
         }
-        return ans;
+        return index;
     }
 }
