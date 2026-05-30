c549244b2cc146545dd74a72c279c76edfc13ce3
anil741635 <anil23hr1a0547@gmail.com>
2026-05-30 12:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (64.09%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,15 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        long ans=1;
-        for(int i=1;i<m;i++) ans=ans*(n-1+i)/i;
-      // return  helper(m,n,0,0);
-      return (int) ans;
+        // long ans=1;
+        // for(int i=1;i<m;i++) ans=ans*(n-1+i)/i;
+        //return (int) ans;
+        int dp[][]=new int[m][n];
+        for(int[] i:dp) Arrays.fill(i,-1);
+       return  helper(m,n,0,0,dp);
     }
-    // public static int helper(int m,int n,int row,int col){
-    //     if(row>=m || col>=n) return 0;
-    //     if((row==m-1) && (col==n-1)) return 1;
-    //     int down=helper(m,n,row+1,col);
-    //     int right=helper(m,n,row,col+1);
-    //     return down+right;
-    // }
+    public static int helper(int m,int n,int row,int col,int[][] dp){
+        if(row>=m || col>=n) return 0;
+        if((row==m-1) && (col==n-1)) return 1;
+        if(dp[row][col]!=-1) return dp[row][col];
+        int down=helper(m,n,row+1,col,dp);
+        int right=helper(m,n,row,col+1,dp);
+        return dp[row][col]=down+right;
+    }
 }
