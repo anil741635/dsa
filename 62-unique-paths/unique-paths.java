647af256aca1e30497a8ed6d84f701dbb728fc48
anil741635 <anil23hr1a0547@gmail.com>
2026-05-30 12:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.08%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,9 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        double ans=1;
-        for(int i=1;i<m;i++){
-            ans=ans*(n-1+i)/i;
-        }
-        return (int) ans;
+        long ans=1;
+        for(int i=1;i<m;i++) ans=ans*(n-1+i)/i;
+      // return  helper(m,n,0,0);
+      return (int) ans;
     }
+    // public static int helper(int m,int n,int row,int col){
+    //     if(row>=m || col>=n) return 0;
+    //     if((row==m-1) && (col==n-1)) return 1;
+    //     int down=helper(m,n,row+1,col);
+    //     int right=helper(m,n,row,col+1);
+    //     return down+right;
+    // }
 }
