3887dc2447fc434c62710bbb01028b1692280b93
anil741635 <anil23hr1a0547@gmail.com>
2026-05-31 20:33:51 +0530

    Time: 1 ms (98.04%) | Memory: 42.5 MB (76.57%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+      int left=1,right=x;
+      int ans=0;
+      while(left<=right){
+        int mid=left+(right-left)/2;
+        if(mid>x / mid) right=mid-1;
+        else {ans=mid;
+            left=mid+1;
+        }
+      }
+      return ans;
+    }
+}
