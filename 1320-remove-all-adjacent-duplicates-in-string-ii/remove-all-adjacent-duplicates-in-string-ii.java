27999c6a7ae8965492d0d7213221131d2f646186
anil741635 <anil23hr1a0547@gmail.com>
2026-06-07 11:38:06 +0530

    Time: 30 ms (46.67%) | Memory: 47.3 MB (54.77%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,30 @@
+class Pair{
+    char ch;
+    int count;
+    Pair(char ch,int count){
+        this.ch=ch;
+        this.count=count;
+    }
+}
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(!st.isEmpty() && st.peek().ch==c){
+                st.peek().count++;
+                if(st.peek().count==k){
+                    st.pop();
+                }
+            }
+            else st.push(new Pair(c,1));
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            Pair p=st.pop();
+            for(int i=0;i<p.count;i++){
+                sb.append(p.ch);
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
