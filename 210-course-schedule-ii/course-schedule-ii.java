3d378785d068c8b16eb4191c7ca59e3b22e9f00d
anil741635 <anil23hr1a0547@gmail.com>
2026-06-14 14:11:24 +0530

    Time: 5 ms (84.11%) | Memory: 46.7 MB (80.77%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,33 +1,32 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int n=numCourses;
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=numCourses;
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        int[] indegre=new int[n];
-        for(int[] edge:prerequisites){
+        int[] indegree=new int[n];
+        int[] res=new int[n];
+        int index=0;
+        for(int edge[]:prerequisites){
             int u=edge[0];
             int v=edge[1];
             adj.get(v).add(u);
-            indegre[u]++;
+            indegree[u]++;
         }
         Queue<Integer> q=new LinkedList<>();
-       int[] result=new int[n];
-       int index=0;
         for(int i=0;i<n;i++){
-            if(indegre[i]==0) q.add(i);
+            if(indegree[i]==0) q.add(i);
         }
         while(!q.isEmpty()){
-            int node=q.poll();
-            result[index++]=node;
-            for(int neighbor:adj.get(node)){
-                indegre[neighbor]--;
-                if(indegre[neighbor]==0) q.add(neighbor);
+            int value=q.poll();
+            res[index++]=value;
+            for(int neighbor:adj.get(value)){
+                indegree[neighbor]--;
+                if(indegree[neighbor]==0)q.add(neighbor);
             }
         }
         if(index!=n) return new int[0];
-        return result;
+        return res;
     }
 }
-   
