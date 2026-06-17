d05a6a2967add3dcbddf8128b0835dcfb378b85f
anil741635 <anil23hr1a0547@gmail.com>
2026-06-17 08:16:06 +0530

    Time: 529 ms (27.60%) | Memory: 43.2 MB (60.83%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,16 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int total=0;
-        for(int i:nums) total+=i;
-        if(total<Math.abs(target)) return 0;
-        int s1=(target+total)/2;
-        if((target + total) % 2 != 0) return 0;
-        int dp[][]=new int[nums.length+1][s1+1];
-        dp[0][0]=1;
-       for(int i=1;i<=nums.length;i++){
-        for(int j=0;j<=s1;j++){
-            dp[i][j]=dp[i-1][j];
-            if(nums[i-1]<=j) dp[i][j]+=dp[i-1][j-nums[i-1]];
+        return helper(nums,target,0,0);
+    }
+    public static int helper(int nums[],int target,int sum,int index){
+        if(index==nums.length){
+            if(target==sum){
+                return 1;
+            }
+            return 0;
         }
-       }
-       return dp[nums.length][s1];
+        int skip=helper(nums,target,sum-nums[index],index+1);
+        int notskip=helper(nums,target,sum+nums[index],index+1);
+        return notskip+skip;
     }
 }
