89b4c675a27232732aae0d74524e3ae7ee8d10e0
anil741635 <anil23hr1a0547@gmail.com>
2026-06-17 11:53:30 +0530

    Time: 32 ms (9.86%) | Memory: 53.6 MB (85.37%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,17 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length(),m=text2.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        int dp[][]=new int[text1.length()][text2.length()];
+        for(int i[]:dp) Arrays.fill(i,-1);
+        return helper(text1,text2,0,0,dp);
+    }
+    public static int helper(String s1,String s2,int index1,int index2,int[][] dp){
+        if(s1.length()==index1 || s2.length()==index2) return 0;
+        if(s1.charAt(index1)==s2.charAt(index2)) {
+            return dp[index1][index2]=1+helper(s1,s2,index1+1,index2+1,dp);
         }
-        return dp[n][m];
-    }  
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+         int skip1=helper(s1,s2,index1+1,index2,dp);
+        int skip2=helper(s1,s2,index1,index2+1,dp);
+        return dp[index1][index2]=Math.max(skip1,skip2);
+    }
 }
