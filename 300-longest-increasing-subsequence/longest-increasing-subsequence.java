0cb2b72a2913e000f1aedd47e6eb95f8c0aa0591
anil741635 <anil23hr1a0547@gmail.com>
2026-06-17 12:20:35 +0530

    Time: 383 ms (10.43%) | Memory: 236.9 MB (18.80%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,1);
-        int maxlen=1;
-        for(int i=0;i<nums.length;i++){
-            for(int prev=0;prev<i;prev++){
-                if(nums[prev]<nums[i]){
-                    dp[i]=Math.max(dp[i],1+dp[prev]);
-                }
-            }
-            maxlen=Math.max(maxlen,dp[i]);
-        }
-        return maxlen;
+        int[][] dp=new int[nums.length][nums.length];
+        for(int i[]:dp) Arrays.fill(i,-1);
+        return helper(nums,0,-1,dp);
+    }
+    public static int helper(int[] nums,int index,int prev,int[][] dp){
+        if(index==nums.length) return 0;
+        if(dp[index][prev+1]!=-1) return dp[index][prev+1];
+        int skip=helper(nums,index+1,prev,dp);
+        int take=0;
+        if(prev==-1 || nums[prev]<nums[index])
+            take=1+helper(nums,index+1,index,dp);
+        return dp[index][prev+1]=Math.max(skip,take);
     }
 }
