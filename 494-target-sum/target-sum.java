67cc5116b85087dbc43c460d6ec6a590dc8c33c5
j anil kumar <anil23hr1a0547@gmail.com>
2026-07-05 19:47:58 +0530

    Time: 8 ms (60.26%) | Memory: 46.8 MB (23.50%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,23 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-       return helper(nums,target,0); 
+        int total=0;
+        for(int i:nums) total+=i;
+        if(total<Math.abs(target)) return 0;
+        int dp[][]=new int[nums.length+1][2*total+1];
+        for(int i[]:dp) Arrays.fill(i,-1);
+       return helper(nums,target,0,dp,total); 
     }
-    public static int helper(int nums[],int target,int ind){
+    public static int helper(int nums[],int target,int ind,int[][] dp,int total){
+        if(target>total || -target>total) return 0;
         if(ind==nums.length){
             if(target==0){
                 return 1;
             }
             else return 0;
         }
-             int   neg=helper(nums,target-nums[ind],ind+1);
-        int pos=helper(nums,target+nums[ind],ind+1);
-        return neg+pos;
+        if(dp[ind][target+total]!=-1) return dp[ind][target+total];
+        int  neg=helper(nums,target-nums[ind],ind+1,dp,total);
+        int pos=helper(nums,target+nums[ind],ind+1,dp,total);
+        return dp[ind][target+total]=neg+pos;
     }
 }
